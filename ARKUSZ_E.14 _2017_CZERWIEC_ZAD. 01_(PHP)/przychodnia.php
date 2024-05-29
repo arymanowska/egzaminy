a71a59ca3a17b01fcd1191bdcb776a7cd5bf5f37
Amela <amelia.rym@wp.pl>
2024-05-29 09:28:49 +0200

    update

---
+++ ARKUSZ_E.14 _2017_CZERWIEC_ZAD. 01_(PHP)/przychodnia.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="przychodnia.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Przychodnia</title>
+</head>
+<body>
+    <div id="baner">
+        <h1>PRAKTYKA LEKARZA RODZINNEGO</h1>
+    </div>
+    <div id="lewy">
+        <h3>LISTA PACJENTÓW</h3>
+        <?php
+        $server = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "przychodnia";
+
+        $conn = mysqli_connect($server, $user, $pass, $db);
+
+        $sql = "SELECT id, imie, nazwisko FROM pacjenci;";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                
+
+                echo "". $row["id"] ." ". $row["imie"] ." ". $row["nazwisko"] ."<br>
+                ";
+            }
+        }
+
+
+        mysqli_close($conn);
+        ?>
+        <br><br>
+        <form action="pacjent.php" method="post">
+            Podaj id:<br>
+            <input type="number" name="id">
+            <input type="submit" value="Pokaż dane" name="submit">
+        </form>
+        <h3>LEKARZE</h3>
+        <ul>
+            <li>pn - śr</li>
+        </ul>
+        <ol>
+            <li>Anna Kwiatkowska</li>
+            <li>Jan Kowalewski</li>
+        </ol>
+        <ul>
+            <li>czw - pt</li>
+        </ul>
+        <ol>
+            <li>Krzysztof Nowak</li>
+        </ol>
+    </div>
+    <div id="prawy">
+        <h2>INFORMACJE SZCZEGÓŁOWE O PACJENCIE</h2>
+        <p>Brak wybranego pacjenta</p>
+    </div>
+    <div id="stopka">
+        <p>utworzone przez: Amelia R</p>
+        <a href="kwerendy.txt">Pobierz plik z kwerendami</a>
+    </div>
+</body>
+</html>
